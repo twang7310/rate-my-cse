f72929ebb5067da868b818ebef3995fe736f6979
jeremynguyen <jeremynguyen593@gmail.com>
2023-10-17 19:28:37 -0700

    Changed the file locations and added header with styling

---
+++ app/src/Homepage/Homepage-components.tsx
@@ -0,0 +1,7 @@
+export const Header: React.FC = () => {
+    return (
+        <div className="header">
+             {/* This is the content of the header */}
+        </div>
+    );
+}
---
+++ app/src/Homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+import {Header} from './Homepage-components';
+import './Homepage.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
