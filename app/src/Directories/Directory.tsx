2a590bf73584841f85bc614dc35818d7d5cc0424
celestinebuendia <celestinembuendia@yahoo.com>
2023-11-29 00:48:34 -0800

    Remove unnecessary import

--- app/src/Directories/Directory.tsx
+++ app/src/Directories/Directory.tsx
@@ -1,7 +1,6 @@
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import './Directory.css'
-import '../Homepage/Homepage.css'
 
 interface ClassListProps {
     classLevelNumber: string;
