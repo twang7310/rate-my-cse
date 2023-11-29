00152191c437fd414c2cbe338d3e334055597806
celestinebuendia <celestinembuendia@yahoo.com>
2023-11-29 00:36:10 -0800

    Made fonts resize with width

--- app/src/Directories/Directory.tsx
+++ app/src/Directories/Directory.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import './Directory.css'
+import '../Homepage/Homepage.css'
 
 interface ClassListProps {
     classLevelNumber: string;
